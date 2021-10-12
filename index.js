657af2b516af1cfa5518583ac57ad13b28e66c67
EloiGry <eloi.grychta@gmail.com>
2021-10-12 16:05:27 +0200

    modif

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 
 function onTaskSubmit() {
-    var div = document.createElement("div");
-        div.classList.add("barre");
-        div.innerHTML = document.getElementById("input").value
-    document.getElementById("main").appendChild(div);
+    var text = document.getElementById("input").value
+    
+    var div = document.getElementById("main");
+    div.classList.add("barre");
+    div.innerHTML = div.innerHTML + ` <div class="inside"> <p class="text">${text}</p> <div class="insideButton"> <button class="mod"> Modifier </button> <button class="x"> <i class="fas fa-trash-alt"></i> </button> </div> </div> `
+                     
 }
 
 
