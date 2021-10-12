5617611a9e0a117a1743ef212bae0eacc6dd98b2
EloiGry <eloi.grychta@gmail.com>
2021-10-12 21:12:06 +0200

    modif

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
+var inmain = document.getElementById("main")
+var array = []
 
 
 function onTaskSubmit() {
-    var text = document.getElementById("input").value
+
+    var tasks = {
+        value : document.getElementById("input").value,
+        status : "To Do"
+    }
+    array.push(tasks)
     
-    var div = document.getElementById("main");
-    div.classList.add("barre");
-    div.innerHTML = div.innerHTML + ` <div class="inside"> <p class="text">${text}</p> <div class="insideButton"> <button class="mod"> Modifier </button> <button class="x"> <i class="fas fa-trash-alt"></i> </button> </div> </div> `
+    
+    
+    array.forEach(function(task) {
+        inmain.innerHTML = inmain.innerHTML + ` <div class="inside"> <p class="text">${task.value}</p> <div class="insideButton"> <button class="mod"> Modifier </button> <button class="x"> <i class="fas fa-trash-alt"></i> </button> </div> </div> `
+      })
+    
                      
 }
+
+
+
+
+// function onButtonClick() {
+
+    
+
+    // .innerHTML = `<ul class="nav nav-tabs"> 
+    //                         <ul class="dropdown-menu">
+    //                             <li><a class="dropdown-item" href="#">Action</a></li>
+    //                             <li><a class="dropdown-item" href="#">Another action</a></li>
+    //                             <li><a class="dropdown-item" href="#">Something else here</a></li>
+    //                         </ul>
+    //                     <ul class="nav nav-tabs">`
+// }
+
 
 
