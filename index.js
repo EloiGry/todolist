d358a311c7c350b9bad76e6bdaa28c51b472245c
EloiGry <eloi.grychta@gmail.com>
2021-10-12 14:05:27 +0200

    modif

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+
 function onTaskSubmit() {
-    
+    var div = document.createElement("div");
+        div.classList.add("barre");
+        div.innerHTML = document.getElementById("input").value
+    document.getElementById("main").appendChild(div);
 }
+
+
