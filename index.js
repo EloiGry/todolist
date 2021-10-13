e3b00f6bf0b6e3b9db011c664761fd8c147d150d
EloiGry <eloi.grychta@gmail.com>
2021-10-13 12:14:05 +0200

    modif

--- index.js
+++ index.js
@@ -12,28 +12,32 @@ function onTaskSubmit() {
     
     inmain.innerHTML = ""
     
-    array.forEach(function(task) {
-        inmain.innerHTML = inmain.innerHTML + ` <div class="inside"> <p class="text">${task.value}</p> <div class="insideButton"> <button class="mod"> Modifier </button> <button class="x"> <i class="fas fa-trash-alt"></i> </button> </div> </div> `
+    array.forEach(function(task, index) {
+        inmain.innerHTML = inmain.innerHTML + ` <div class="inside" id="delete"> <p class="text">${task.value}</p> <div class="insideButton"> <button class="mod" onclick = "modify(${index})"> Modifier </button> <button class="x" onclick = "removeItem(${index})"> <i class="fas fa-trash-alt"></i> </button> </div> </div> `
       })
     
-                     
+     console.log(array)                
 }
 
+function modify() {
+ var edit = document.createElement("div")
+ array.forEach(function(task, index) {
+ edit.innerHTML = `<ul class="nav nav-tabs"> 
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                     </ul>
+                    <ul class="nav nav-tabs">`
+}
+ )}
 
 
-
-// function onButtonClick() {
-
-    
-
-//     .innerHTML = `<ul class="nav nav-tabs"> 
-//                             <ul class="dropdown-menu">
-//                                 <li><a class="dropdown-item" href="#">Action</a></li>
-//                                 <li><a class="dropdown-item" href="#">Another action</a></li>
-//                                 <li><a class="dropdown-item" href="#">Something else here</a></li>
-//                             </ul>
-//                         <ul class="nav nav-tabs">`
-// }
-
-
+function removeItem(index) {
+    array.splice(index, 1)
+    inmain.innerHTML = ""
+    array.forEach(function(task, index) {
+        inmain.innerHTML = inmain.innerHTML + ` <div class="inside" id="delete"> <p class="text">${task.value}</p> <div class="insideButton"> <button class="mod"> Modifier </button> <button class="x" onclick = "removeItem(${index})"> <i class="fas fa-trash-alt"></i> </button> </div> </div> `
+      })
+}
 
