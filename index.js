4cf82fd571c7a19bc97f035214f46364a1ce4b4b
Syrine Lahouel <syrinelahouel@Syrines-MacBook-Pro.local>
2021-10-13 12:13:13 +0200

    randomtask

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-module.export = onTaskSubmit
 
 var inmain = document.getElementById("main")
 var array = []
+var randomBtn = document.getElementById("random");
+var list = ["manger","dormir","boire","pleurer","douche","nettoyer"]
+list.innerHTML
+
 
 
 function onTaskSubmit() {
@@ -21,6 +24,24 @@ function onTaskSubmit() {
                      
 }
 
+function randomTask() {
+    // generer chiffre aleatoire 
+    // recuperer element qui correspond au chiffre dans le tableau list
+    // ajouter a la valeur du task
+    var tasks = {
+        value : "test",
+        status : "To Do"
+    }
+
+    array.push(tasks)
+    
+    inmain.innerHTML = ""
+    
+    array.forEach(function(task) {
+        inmain.innerHTML = inmain.innerHTML + ` <div class="inside"> <p class="text">${task.value}</p> <div class="insideButton"> <button class="mod"> Modifier </button> <button class="x"> <i class="fas fa-trash-alt"></i> </button> </div> </div> `
+    })
+}
+
 
 
 
@@ -37,4 +58,3 @@ function onTaskSubmit() {
 //                             </ul>
 //                         <ul class="nav nav-tabs">`
 // }
-
