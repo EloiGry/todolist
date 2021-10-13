b9ef2c67182e4cb6603d51a4d85419182c2cd7c5
EloiGry <eloi.grychta@gmail.com>
2021-10-13 16:49:36 +0200

    modif

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ var array = []
 
 
 
+
+
 function onTaskSubmit() {
+
 
     var tasks = {
         value : document.getElementById("input").value,
         status : "To Do"
     }
+
     array.push(tasks)
     
     inmain.innerHTML = ""
@@ -20,6 +24,7 @@ function onTaskSubmit() {
             <div class="inside" id="delete"> 
                 <p class="text">${task.value}</p> 
                 <div class="insideButton"> 
+                    <p class="write"> <i class="fas fa-pencil-alt" id="write${indexElement}" onclick = "write(${indexElement})"></i> </p> 
                     <button class="mod" id="modif-${index}" onclick = "modify(${index})"> Modifier </button> 
                     <button class="x" onclick = "removeItem(${index})"> <i class="fas fa-trash-alt"></i> 
                     </button> 
@@ -27,21 +32,48 @@ function onTaskSubmit() {
             </div> 
         `
       })
-    
-     console.log(array)                
+                  
+}
+function removeItem(index) {
+    array.splice(index, 1)
+    inmain.innerHTML = ""
+    array.forEach(function(task, index) {
+        inmain.innerHTML = inmain.innerHTML + ` <div class="inside" id="delete"> <p class="text">${task.value}</p> <div class="insideButton"> <button class="mod"> Modifier </button> <button class="x" onclick = "removeItem(${index})"> <i class="fas fa-trash-alt"></i> </button> </div> </div> `
+      })
 }
 
 function modify(index) {
     var choice = document.getElementById(`modif-${index}`)
     choice.innerHTML = `<select>
-                                <option value="toDo">Do</option>
-                                <option value="Doing">Doing</option>
-                                <option value="Done">Done</option>
-                            </select>`
+                                <button onclick="firstChoice"><option value="toDo">  To Do  </option><button>
+                                <button onclick="secondChoice"><option value="Doing">  Doing </option><button>
+                                <button onclick="thirdChoice"> <option value="Done">  Done </option><button>
+                        </select>`
+
   }
 
+  function write(index) {
+    var rescribe = document.getElementById(`write-${index}`)
+    rescribe.innerHTML = `<form>
+                            <input type="texte" placeholder : "New name of the task">
+                         </form>`
+}
 
 
+// function firstChoice (index) {
+//     var choice = document.getElementById(`modif-${index}`)
+//     choice.innerHTML = "To Do"
+// }
+
+// function secondChoice (index) {
+//     var choice = document.getElementById(`modif-${index}`)
+//     choice.innerHTML = "Doing"
+// }
+
+// function thirdChoice (index) {
+//     var choice = document.getElementById(`modif-${index}`)
+//     choice.innerHTML = "Done"
+// }
 
 
 
