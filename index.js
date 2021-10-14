500ac4b83760be57a30e226b8c52367a2b527ebe
EloiGry <eloi.grychta@gmail.com>
2021-10-14 11:31:46 +0200

    modif

--- index.js
+++ index.js
@@ -2,21 +2,8 @@
 var inmain = document.getElementById("main")
 var array = []
 
-
-
-
-function onTaskSubmit() {
-
-    var tasks = {
-        value : document.getElementById("input").value,
-        status : "To Do"
-    }
-    array.push(tasks)
+function displayList () {
     inmain.innerHTML = ""
-    
-              
-
-
 
     array.forEach(function(task, index) {
         inmain.innerHTML = inmain.innerHTML + ` 
@@ -24,13 +11,29 @@ function onTaskSubmit() {
                 <p class="text">${task.value}</p> 
                 <div class="insideButton"> 
                     <p class="write" id="write${index}"> <i class="fas fa-pencil-alt" onclick="stylo(${index})"></i> </p> 
-                    <button class="mod" id="modif-${index}" onclick="modify(${index})"> Modifier </button> 
+                    <select class="select" onChange="onSelectChange(${index})">
+                        <option ${task.status === "to do" ? 'selected' : ''} value="to do">To do</option>
+                        <option ${task.status === "doing" ? 'selected' : ''} value="doing">Doing</option>
+                        <option ${task.status === "done" ? 'selected' : ''} value="done">Done</option>
+                    </select> 
                     <button class="x" onclick = "removeItem(${index})"> <i class="fas fa-trash-alt"></i> 
                     </button>
                 </div> 
             </div> 
             `
       })   
+}
+
+
+function onTaskSubmit() {
+
+    var task = {
+        value : document.getElementById("input").value,
+        status : "To Do"
+    }
+    array.push(task)
+    displayList ()
+   
 }         
    
                   
@@ -40,46 +43,32 @@ function removeItem(index) {
     inmain.innerHTML = ""
     array.forEach(function(task, index) {
         inmain.innerHTML = inmain.innerHTML + ` <div class="inside" id="delete"> <p class="text">${task.value}</p> <div class="insideButton"> <button class="mod"> Modifier </button> <button class="x" onclick = "removeItem(${index})"> <i class="fas fa-trash-alt"></i> </button> </div> </div> `
-      })   
+      })  
+      displayList () 
 }
                                  
 
-
-function modify(index) {
-    var choice = document.getElementById(`modif-${index}`)
-    choice.innerHTML = `<select>
-                                <button onclick="firstChoice"><option value="toDo">  To Do  </option><button>
-                                <button onclick="secondChoice"><option value="Doing">  Doing </option><button>
-                                <button onclick="thirdChoice"> <option value="Done">  Done </option><button>
-                        </select>`
-  }
-
   function stylo(index) {
     var rescribe = document.getElementById(`write${index}`)
-    rescribe.innerHTML = `  <form>
-                            <input type="texte" placeholder : "New name of the task">
-                            </form>`
+    rescribe.innerHTML = ` 
+                            <input id="inputChange" type="texte" placeholder : "New name of the task">
+                            <button onclick="addNewTask(${index})"> Change </button>
+                        `
+}
 
+function addNewTask (index) {
+    array[index].value = document.getElementById("inputChange").value
+    displayList()
 }
 
 
 
+function onSelectChange (index) {
+    var select = document.getElementsByClassName("select")[index]
+    array[index].status = select.value
+}
 
 
-// function firstChoice (index) {
-//     var choice = document.getElementById(`modif-${index}`)
-//     choice.innerHTML = "To Do"
-// }
-
-// function secondChoice (index) {
-//     var choice = document.getElementById(`modif-${index}`)
-//     choice.innerHTML = "Doing"
-// }
-
-// function thirdChoice (index) {
-//     var choice = document.getElementById(`modif-${index}`)
-//     choice.innerHTML = "Done"
-// }
 
 
 
