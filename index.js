ac3c64311ba1a09e8cb400617783a617557862aa
EloiGry <eloi.grychta@gmail.com>
2021-11-06 10:59:01 +0100

    modif

--- index.js
+++ index.js
@@ -1,109 +1,152 @@
 
-var inmain = document.getElementById("main")
-var array = []
-// Add //
-function displayList (array) {
-    inmain.innerHTML = ""
+var descriptionInput = document.getElementById("input")
+var priorityInput = document.getElementById("priority-input")
+var list = document.getElementById("list")
 
-    array.forEach(function(task, index) {
-        inmain.innerHTML = inmain.innerHTML + ` 
-            <div class="inside" id="delete">
-                <p class="text">${task.value}</p> 
-                <div class="insideButton"> 
-                    <p class="write" id="write${index}"> <i class="fas fa-pencil-alt" onclick="stylo(${index})"></i> </p> 
-                    <select class="stars" onChange="onStarChange(${index})">
-                        <option ${task.priority === 1 ? 'selected' : ''} value="1">&#9734</option>
-                        <option ${task.priority === 2 ? 'selected' : ''} value="2">&#9734&#9734</option>
-                        <option ${task.priority === 3 ? 'selected' : ''} value="3">&#9734&#9734&#9734</option>
-                        <option ${task.priority === 4 ? 'selected' : ''} value="4">&#9734&#9734&#9734&#9734</option>
-                        <option ${task.priority === 5 ? 'selected' : ''} value="5">&#9734&#9734&#9734&#9734&#9734</option>
-                    </select>
-
-                    <select class="select" onChange="onSelectChange(${index})">
-                        <option ${task.status === "To do" ? 'selected' : ''} value="To do">To do</option>
-                        <option ${task.status === "Doing" ? 'selected' : ''} value="Doing">Doing</option>
-                        <option ${task.status === "Done" ? 'selected' : ''} value="Done">Done</option>
-                    </select> 
-                    <button class="x" onclick = "removeItem(${index})"> <i class="fas fa-trash-alt"></i> 
-                    </button>
-                </div> 
-            </div> 
-            `
-      })   
-}
-
-
+var tasks = []
 
 function onTaskSubmit() {
+  var task = {
+    value: descriptionInput.value,
+    status: "to do",
+    priority: priorityInput.value
+  }
+  
+  tasks.push(task)
+  displayList(tasks)
 
-    var task = {
-        value : document.getElementById("input").value,
-        status : "To Do",
-        priority : 1
-    }
-    array.push(task)
-    displayList (array)
-   
-}         
-   
-                  
-function removeItem(index) {
-    array.splice(index, 1)
-    inmain.innerHTML = ""
-    array.forEach(function(task, index) {
-        inmain.innerHTML = inmain.innerHTML + ` <div class="inside" id="delete"> <p class="text">${task.value}</p> <div class="insideButton"> <button class="mod"> Modifier </button> <button class="x" onclick = "removeItem(${index})"> <i class="fas fa-trash-alt"></i> </button> </div> </div> `
-      })  
-      displayList (array) 
-}
-                                 
-
-  function stylo(index) {
-    var rescribe = document.getElementById(`write${index}`)
-    rescribe.innerHTML = ` 
-                            <input id="inputChange" type="texte" placeholder="New name of the task">
-                            <button onclick="addNewTask(${index})"> Change </button>
-                        `
+  descriptionInput.value = ""
+  priorityInput.value = ""
 }
 
-function addNewTask (index) {
-    array[index].value = document.getElementById("inputChange").value
-    displayList(array)
+function displayList(array) {
+  list.innerHTML = ``
+  
+  array.forEach(function(task, index) {
+    var statusClass = getStatusClass(task.status)
+
+    list.innerHTML = list.innerHTML + `
+      <li id="item-${index}" class="list-group-item">
+        <div class="row justify-between py-2">
+          <div class="col-5 d-flex align-items-center">
+            <span>${task.value}</span>
+          </div>
+          <div class="col-2 d-flex align-items-center">
+            <b class="${statusClass} opacity-75">${task.status}</b>
+          </div>
+          <div class="col-2 d-flex align-items-center">
+             <span>${task.priority !== 0 ? task.priority : ''}</span>
+           </div>
+          <div class="col-3 d-flex justify-content-end">
+            <button type="button" class="btn btn-outline-primary me-3" onclick="edit(${index})">Modifier</button>
+            <button type="button" class="btn btn-outline-danger" onclick="remove(${index})">Supprimer</button>
+          </div>
+        </div>
+      </li>
+    `
+  })
 }
 
-
-function onSelectChange (index) {
-    var select = document.getElementsByClassName("select")[index]
-    array[index].status = select.value
+function remove(index) {
+  tasks.splice(index, 1)
+  displayList(tasks)
 }
 
-function filter(status){
-        var filterToDo = array.filter(function(task){
-            return task.status === status || status === "All"
-        })
-        displayList(filterToDo)
+function edit(index) {
+  var taskLi = document.getElementById(`item-${index}`)
+  var task = tasks[index]
+  
+  taskLi.innerHTML = `
+    <div class="row justify-between py-2">
+      <div class="col-5 d-flex align-items-center">
+        <input id="item-${index}-input" type="text" class="form-control" value="${task.value}" autofocus />
+      </div>
+      <div class="col-2 d-flex align-items-center">
+        <select id="item-${index}-select" class="form-select" aria-label="Select status">
+          <option ${task.status === 'to do' ? 'selected' : ''} value="to do">To do</option>
+          <option ${task.status === 'doing' ? 'selected' : ''} value="doing">Doing</option>
+          <option ${task.status === 'done' ? 'selected' : ''} value="done">Done</option>
+        </select>
+      </div>
+      <div class="col-2 d-flex align-items-center">
+        <input id="item-${index}-priority" type="number" class="form-control" value="${task.priority}" min="0" max="5" />
+      </div>
+      <div class="col-3 d-flex justify-content-end">
+        <button type="button" class="btn btn-outline-danger me-3" onclick="displayList()">Annuler</button>
+        <button type="button" class="btn btn-outline-success" onclick="modify(${index})">Valider</button>
+      </div>
+    </div> 
+  `
 }
 
-function onStarChange (index) {
-    var stars = document.getElementsByClassName("stars")[index]
-    array[index].priority = Number(stars.value)
-    console.log(array);
+function modify(index) {
+  var input = document.getElementById(`item-${index}-input`)
+  var select = document.getElementById(`item-${index}-select`)
+  var singlePriorityInput = document.getElementById(`item-${index}-priority`)
+  tasks[index].value = input.value
+  tasks[index].status = select.value
+  tasks[index].priority = Number(singlePriorityInput.value)
+  displayList(tasks)
 }
 
-function priorityTask (){
-    var filterTask = array.sort((a, b) => b.priority - a.priority)
-    displayList(filterTask)
+function getStatusClass(status) {
+  if (status === "to do") {
+    return "text-danger"
+  } else if (status === "doing") {
+     return "text-warning"
+  } else {
+    return "text-success"
+  }
 }
 
-function randomTask() {
-    
-    var list = ["manger","dormir","boire","sport","douche","nettoyer"]
-    var random = list[Math.floor(Math.random()*list.length)];
-    var tasks = {
-        value : random,
-        status : "To Do"    
-    };
-    array.push(tasks)
-    displayList(array)
+function generateRandomTask() {
+  var randomTasks = [
+    "Faire du yoga",
+    "Aller au cinéma",
+    "Manger la meilleure pizza de sa vie",
+    "100 - 0 au basket",
+    "Faire les courses"
+  ]
+
+  var task = {
+    value: randomTasks[Math.floor(Math.random() * randomTasks.length)],
+    status: "to do"
+  }
+
+  tasks.push(task)
+  displayList(tasks)
+}
+
+function filter(status) {
+  var filteredTasks = tasks.filter(function(task) {
+    return task.status === status || status === "all"
+  })
+  
+  displayList(filteredTasks)
+}
+
+function priorityKeypress(event) {
+  var char = String.fromCharCode(event.charCode)
+
+  if (Number(char) === NaN) {
+    priorityInput.value = ""
+  }
+}
+
+function orderPriority(operator) {
+  var sortedTasks = []
+
+  if (operator === "+") {
+    sortedTasks = tasks.sort(function(a, b) {
+      return b.priority - a.priority
+    })
+  } else {
+    sortedTasks = tasks.sort(function(a, b) {
+      return a.priority - b.priority
+    })
+  }
+
+  displayList(sortedTasks)
 }
 
 
